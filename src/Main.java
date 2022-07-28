0f2cee291608115f9b948c10e3180d94d11aac57
NoelGiron <mngiron@hotmail.com>
2022-07-27 22:37:50 -0600

    se creo un scanner y metodo para leer numeros

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Main {
+    Scanner entradaTeclado = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-    
+    int leerNumero(String mensaje){
+        try{
+            int numero = entradaTeclado.nextInt();
+            entradaTeclado.nextLine();
+            return numero;
+        }catch(InputMismatchException exception){
+            return -1;
+        }
+    }
 }
