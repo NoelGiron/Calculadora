ec616031b5568137d7da63c04086eae49b0ff686
NoelGiron <mngiron@hotmail.com>
2022-07-27 22:52:41 -0600

    cambio a metodo static scanner y leerNumero

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
-    Scanner entradaTeclado = new Scanner(System.in);
+    static Scanner entradaTeclado = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Menu");
+        int opcion = leerNumero("Seleccione una opcion \n 1)Aritmetica \n 2)Trigonometria \n 3)Estadistica \n 4)Calculo \n 5)Salir");
+        
     }
-    int leerNumero(String mensaje){
+    static int leerNumero(String mensaje){
+        System.out.println(mensaje);
         try{
             int numero = entradaTeclado.nextInt();
             entradaTeclado.nextLine();
             return numero;
         }catch(InputMismatchException exception){
+            System.out.println("Dato no valido");
+            entradaTeclado.nextLine();
             return -1;
         }
     }
