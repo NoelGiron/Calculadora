34f0d16629a619dbe49c7bafbbc603f95338b215
NoelGiron <mngiron@hotmail.com>
2022-08-13 17:49:55 -0600

    datos guardados

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-//import java.util.function.DoubleToLongFunction;
 import java.lang.Math;
 public class Main {
     //escaner general
